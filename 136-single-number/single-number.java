a29b96eb2c57d7cf3c062b0ac701b56debbe745c
batman18-art <piyushpatil20067385@gmail.com>
2026-09-26 21:34:59 +0530

    Time: 10 ms (26.74%) | Memory: 47.7 MB (7.31%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+         HashSet<Integer> hs=new HashSet<>();
+         for(int i=0;i<nums.length;i++){
+            if(!hs.add(nums[i])){
+                hs.remove(nums[i]);
+            }
+         }
+          return hs.iterator().next();
+    }
+}
